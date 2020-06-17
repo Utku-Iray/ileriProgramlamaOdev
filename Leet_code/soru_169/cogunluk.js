ea913adbc142f7b73f119ff313a3c32f9d3e2b02
utkuiray <57593553+utkuiray@users.noreply.github.com>
2020-06-18 01:51:23 +0300

    Revert "fddfg"
    This reverts commit 968d2ce9c50d47aed0e87e09c68d3e332afe5c16.

--- Leet_code/soru_169/cogunluk.js
+++
@@ -1,7 +0,0 @@
-var majorityElement = function(nums) {
-    
-    nums.sort((a,b) => a - b);
-    return nums[Math.floor(nums.length/2)];
-}; 
-console.log(majorityElement([3,2,3]))
-console.log(majorityElement([2,2,1,1,1,2,2]))
