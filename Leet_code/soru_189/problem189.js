968d2ce9c50d47aed0e87e09c68d3e332afe5c16
utkuiray <57593553+utkuiray@users.noreply.github.com>
2020-06-18 01:51:01 +0300

    fddfg

---
+++ Leet_code/soru_189/problem189.js
@@ -0,0 +1,16 @@
+var rotate = function (nums, k) {
+    console.log('## INPUT: ', nums, 'steps=', k);
+    for (let j = 0; j < k; j++) {
+        let temp = nums[nums.length - 1];
+        for (let i = nums.length - 1; i > 0; i--) {
+            // console.log("i=", i);
+            nums[i] = nums[i - 1];
+        }
+        nums[0] = temp;
+        console.log(' -------> ', nums, ', ROUND ', j);
+    }
+};
+
+let input = [1, 2, 3, 4, 5, 6, 7], k = 3;
+
+rotate(input, k);
